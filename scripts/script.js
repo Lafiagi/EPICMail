81187bbe321fe594d79a6d088d5b55b3db0dcf53
Lafiagi <skillfullmallam@gmail.com>
2019-04-04 07:43:07 +0100

    initial commit for the navigation page

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ if(document.querySelector("title").text === "Reset an EPIC Mail Account Password
   document.querySelector("h3#form-header").style.fontSize = "1.7em";
   document.querySelector("h3#form-header").style.marginTop = "80px";
 }
+function change(){
+    console.log("Read Messages")
+}
+
+function openNav(){
+  let sideBar = document.querySelector("#sidebar");
+  sideBar.style.display = "inline" 
+}
+function closeNav(){
+  let sideBar = document.querySelector("#sidebar");
+  sideBar.style.display = "none" 
+}
